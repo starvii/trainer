d7dbba8590920fcc1d8a72d672f17b7289914317
starvii <starvii@gmail.com>
2019-02-14 01:58:21 +0800

    强化点数修改

--- trainer/dw9/DW9Trainer.Designer.cs
+++ trainer/dw9/DW9Trainer.Designer.cs
@@ -29,16 +29,20 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_points_save = new System.Windows.Forms.Button();
+            this.lbl_points_title = new System.Windows.Forms.Label();
+            this.lbl_character = new System.Windows.Forms.Label();
+            this.lbl_character_title = new System.Windows.Forms.Label();
+            this.lbl_status = new System.Windows.Forms.Label();
             this.lbl_status_title = new System.Windows.Forms.Label();
             this.btn_status = new System.Windows.Forms.Button();
-            this.lbl_status = new System.Windows.Forms.Label();
-            this.lbl_character_title = new System.Windows.Forms.Label();
-            this.lbl_character = new System.Windows.Forms.Label();
             this.btn_read_character = new System.Windows.Forms.Button();
-            this.lbl_points_title = new System.Windows.Forms.Label();
             this.tb_points = new System.Windows.Forms.TextBox();
             this.btn_points_load = new System.Windows.Forms.Button();
-            this.btn_points_save = new System.Windows.Forms.Button();
+            this.lbl_money_title = new System.Windows.Forms.Label();
+            this.tb_money = new System.Windows.Forms.TextBox();
+            this.btn_money_load = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,6 +53,10 @@
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel.Controls.Add(this.button1, 3, 3);
+            this.tableLayoutPanel.Controls.Add(this.btn_money_load, 2, 3);
+            this.tableLayoutPanel.Controls.Add(this.tb_money, 1, 3);
+            this.tableLayoutPanel.Controls.Add(this.lbl_money_title, 0, 3);
             this.tableLayoutPanel.Controls.Add(this.btn_points_save, 3, 2);
             this.tableLayoutPanel.Controls.Add(this.lbl_points_title, 0, 2);
             this.tableLayoutPanel.Controls.Add(this.lbl_character, 1, 1);
@@ -62,13 +70,63 @@
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
-            this.tableLayoutPanel.RowCount = 4;
+            this.tableLayoutPanel.RowCount = 5;
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel.Size = new System.Drawing.Size(895, 448);
             this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // btn_points_save
+            // 
+            this.btn_points_save.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_points_save.Location = new System.Drawing.Point(807, 123);
+            this.btn_points_save.Name = "btn_points_save";
+            this.btn_points_save.Size = new System.Drawing.Size(85, 34);
+            this.btn_points_save.TabIndex = 9;
+            this.btn_points_save.Text = "保存";
+            this.btn_points_save.UseVisualStyleBackColor = true;
+            this.btn_points_save.Click += new System.EventHandler(this.btn_points_save_Click);
+            // 
+            // lbl_points_title
+            // 
+            this.lbl_points_title.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_points_title.Location = new System.Drawing.Point(3, 120);
+            this.lbl_points_title.Name = "lbl_points_title";
+            this.lbl_points_title.Size = new System.Drawing.Size(83, 40);
+            this.lbl_points_title.TabIndex = 6;
+            this.lbl_points_title.Text = "强化点数";
+            this.lbl_points_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_character
+            // 
+            this.lbl_character.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_character.Location = new System.Drawing.Point(92, 60);
+            this.lbl_character.Name = "lbl_character";
+            this.lbl_character.Size = new System.Drawing.Size(620, 60);
+            this.lbl_character.TabIndex = 4;
+            this.lbl_character.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_character_title
+            // 
+            this.lbl_character_title.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_character_title.Location = new System.Drawing.Point(3, 60);
+            this.lbl_character_title.Name = "lbl_character_title";
+            this.lbl_character_title.Size = new System.Drawing.Size(83, 60);
+            this.lbl_character_title.TabIndex = 3;
+            this.lbl_character_title.Text = "当前人物";
+            this.lbl_character_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_status.Location = new System.Drawing.Point(92, 0);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(620, 60);
+            this.lbl_status.TabIndex = 2;
+            this.lbl_status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lbl_status_title
             // 
@@ -92,34 +150,6 @@
             this.btn_status.UseVisualStyleBackColor = true;
             this.btn_status.Click += new System.EventHandler(this.btn_status_Click);
             // 
-            // lbl_status
-            // 
-            this.lbl_status.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lbl_status.Location = new System.Drawing.Point(92, 0);
-            this.lbl_status.Name = "lbl_status";
-            this.lbl_status.Size = new System.Drawing.Size(620, 60);
-            this.lbl_status.TabIndex = 2;
-            this.lbl_status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lbl_character_title
-            // 
-            this.lbl_character_title.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lbl_character_title.Location = new System.Drawing.Point(3, 60);
-            this.lbl_character_title.Name = "lbl_character_title";
-            this.lbl_character_title.Size = new System.Drawing.Size(83, 60);
-            this.lbl_character_title.TabIndex = 3;
-            this.lbl_character_title.Text = "当前人物";
-            this.lbl_character_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lbl_character
-            // 
-            this.lbl_character.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lbl_character.Location = new System.Drawing.Point(92, 60);
-            this.lbl_character.Name = "lbl_character";
-            this.lbl_character.Size = new System.Drawing.Size(620, 60);
-            this.lbl_character.TabIndex = 4;
-            this.lbl_character.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // btn_read_character
             // 
             this.tableLayoutPanel.SetColumnSpan(this.btn_read_character, 2);
@@ -131,16 +161,6 @@
             this.btn_read_character.Text = "加载人物ID";
             this.btn_read_character.UseVisualStyleBackColor = true;
             this.btn_read_character.Click += new System.EventHandler(this.btn_read_character_Click);
-            // 
-            // lbl_points_title
-            // 
-            this.lbl_points_title.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lbl_points_title.Location = new System.Drawing.Point(3, 120);
-            this.lbl_points_title.Name = "lbl_points_title";
-            this.lbl_points_title.Size = new System.Drawing.Size(83, 40);
-            this.lbl_points_title.TabIndex = 6;
-            this.lbl_points_title.Text = "当前人物";
-            this.lbl_points_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // tb_points
             // 
@@ -164,16 +184,46 @@
             this.btn_points_load.UseVisualStyleBackColor = true;
             this.btn_points_load.Click += new System.EventHandler(this.btn_points_load_Click);
             // 
-            // btn_points_save
+            // lbl_money_title
             // 
-            this.btn_points_save.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_points_save.Location = new System.Drawing.Point(807, 123);
-            this.btn_points_save.Name = "btn_points_save";
-            this.btn_points_save.Size = new System.Drawing.Size(85, 34);
-            this.btn_points_save.TabIndex = 9;
-            this.btn_points_save.Text = "保存";
-            this.btn_points_save.UseVisualStyleBackColor = true;
-            this.btn_points_save.Click += new System.EventHandler(this.btn_points_save_Click);
+            this.lbl_money_title.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_money_title.Location = new System.Drawing.Point(3, 160);
+            this.lbl_money_title.Name = "lbl_money_title";
+            this.lbl_money_title.Size = new System.Drawing.Size(83, 40);
+            this.lbl_money_title.TabIndex = 10;
+            this.lbl_money_title.Text = "金钱";
+            this.lbl_money_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb_money
+            // 
+            this.tb_money.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_money.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb_money.HideSelection = false;
+            this.tb_money.Location = new System.Drawing.Point(92, 163);
+            this.tb_money.Name = "tb_money";
+            this.tb_money.Size = new System.Drawing.Size(620, 33);
+            this.tb_money.TabIndex = 11;
+            this.tb_money.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_money_load
+            // 
+            this.btn_money_load.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_money_load.Location = new System.Drawing.Point(718, 163);
+            this.btn_money_load.Name = "btn_money_load";
+            this.btn_money_load.Size = new System.Drawing.Size(83, 34);
+            this.btn_money_load.TabIndex = 12;
+            this.btn_money_load.Text = "加载";
+            this.btn_money_load.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(807, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 34);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "保存";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // DW9Trainer
             // 
@@ -201,9 +251,13 @@
         private System.Windows.Forms.Label lbl_character;
         private System.Windows.Forms.Button btn_read_character;
         private System.Windows.Forms.Button btn_points_save;
-        private System.Windows.Forms.Label lbl_points_title;
         private System.Windows.Forms.TextBox tb_points;
         private System.Windows.Forms.Button btn_points_load;
+        private System.Windows.Forms.Label lbl_points_title;
+        private System.Windows.Forms.TextBox tb_money;
+        private System.Windows.Forms.Label lbl_money_title;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_money_load;
     }
 }
 
